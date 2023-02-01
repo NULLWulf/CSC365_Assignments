87ffd22fc4631af857eacad49fba8c8438cc9956
Nathan Wolf <nwolf2@oswego.edu>
2023-01-31 20:18:54 -0500

    modified gui side

---
+++ js/homepage.js
@@ -0,0 +1,14 @@
+const searchButton = document.getElementById("searchButton");
+const searchBox = document.getElementById("searchBox");
+const label1 = document.getElementById("label1");
+const label2 = document.getElementById("label2");
+
+searchButton.addEventListener("click", function () {
+    const searchTerm = searchBox.value;
+    fetch(`https://api.example.com/search?q=${searchTerm}`)
+        .then((response) => response.json())
+        .then((data) => {
+            label1.innerHTML = data.label1;
+            label2.innerHTML = data.label2;
+        });
+});
