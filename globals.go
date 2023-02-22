e3a2987bbcb16f717e87c81180462585655ae249
Nathan Wolf <nwolf2@oswego.edu>
2023-02-22 10:21:30 -0500

    added globals and json file processing go

---
+++ globals.go
@@ -0,0 +1,51 @@
+package main
+
+type Business struct {
+	BusinessID       string             `json:"business_id"`
+	Name             string             `json:"name"`
+	City             string             `json:"city"`
+	State            string             `json:"state"`
+	Stars            float32            `json:"stars"`
+	ReviewCount      int                `json:"review_count"`
+	IsOpen           int                `json:"is_open"`
+	Categories       string             `json:"categories"`
+	CategoriesArr    []string           `json:"categories_arr" nil:"true"`
+	ReviewTermsCount map[string]int     `json:"review_terms_count"`
+	TermCountTotal   int                `json:"term_count_total"`
+	TermFrequency    map[string]float32 `json:"term_frequency"`
+	TfIdf            map[string]float32 `json:"tf_idf"`
+}
+
+type Review struct {
+	ReviewID   string  `json:"review_id"`
+	UserID     string  `json:"user_id"`
+	BusinessID string  `json:"business_id"`
+	Stars      float32 `json:"stars"`
+	Text       string  `json:"text"`
+}
+
+type mapEntry struct {
+	key   string
+	value float32
+}
+
+type program struct {
+	Name        string
+	DisplayName string
+	Description string
+}
+
+type bizTuple struct {
+	BusinessName string `json:"business_name"`
+	BusinessID   string `json:"business_id"`
+}
+
+var (
+	Businesses            = make(map[string]Business)
+	TermDocumentFrequency = make(map[string]int)
+	ReviewTotal           = 25000
+	BusinessKeyMap        []string
+	ReviewCount           = 100
+	RelatibilityMod       = 0.10
+	TermKeyMap            = make(map[string][]string)
+)
---
+++ jsonFileProcessing.go
@@ -0,0 +1,100 @@
+package main
+
+import (
+	"bufio"
+	"encoding/json"
+	"log"
+	"os"
+	"strings"
+
+	"github.com/bbalet/stopwords"
+)
+
+// Reads Businesses JSON data
+func readBusinessesJson() {
+	log.Println("Loading Business JSON data...")
+	Businesses = make(map[string]Business) // Instantiate global businesses map
+	// Read the file containing business information
+	file, err := os.ReadFile(businessPath) // Reads entire file to file object
+	if err != nil {
+		log.Fatal(err)
+	}
+	// Loop through the file object, decoding each business object and adding it to the array
+	BusinessKeyMap = make([]string, 0)
+	for i := 0; i < len(file); {
+		var business Business // temporary Business variable
+		j := i
+		for j < len(file) && file[j] != '\n' {
+			j++
+		}
+		err := json.Unmarshal(file[i:j], &business)
+		if err != nil {
+			log.Println("Business ignored: ", err)
+		} else {
+			// Get businesses
+			if business.IsOpen != 0 && strings.Contains(business.Categories, "Restaurants") && business.ReviewCount > ReviewCount {
+				business.CategoriesArr = strings.Split(business.Categories, ", ")
+				business.ReviewTermsCount = make(map[string]int)
+				Businesses[business.BusinessID] = business
+				BusinessKeyMap = append(BusinessKeyMap, business.BusinessID)
+			}
+		}
+		i = j + 1
+	}
+	log.Println("Businesses Loaded: ", len(Businesses))
+
+}
+
+// Reads through the reviews JSON file.  When a review is found for a bussiness it removes
+// stop words from the text, splits the text into an array, and adds the term to a raw
+// count frequency map for the review's respective business.
+func readReviewsJsonScannner() {
+	log.Println("Reading review JSON data...")
+
+	file, err := os.Open(reviewJsonPath)
+	if err != nil {
+		log.Println("Error opening file:", err)
+		return
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	t := 0
+	for scanner.Scan() {
+		var review Review
+
+		err := json.Unmarshal(scanner.Bytes(), &review)
+		if err != nil {
+			log.Println("Review ignored: ", err)
+			continue
+		}
+
+		for _, b := range Businesses {
+			if b.BusinessID == review.BusinessID {
+				// Remove stop words and split by spaces
+				tTerms := strings.Split(stopwords.CleanString(review.Text, "en", true), " ")
+				for _, term := range tTerms {
+					ptr := Businesses[b.BusinessID]
+					if ptr.ReviewTermsCount == nil {
+						ptr.ReviewTermsCount = make(map[string]int)
+					}
+					ptr.ReviewTermsCount[term]++
+				}
+				t++
+				break
+			}
+		}
+
+		// Total on total possible rules that can be added to a business.
+		// Can be modified globally
+		if t == ReviewTotal {
+			break
+		}
+	}
+
+	if err := scanner.Err(); err != nil {
+		log.Println("Error reading file:", err)
+	}
+
+	log.Printf("Reviews Loading: %d.  Businesses before removal of nulls: %d", ReviewTotal, len(Businesses))
+}
