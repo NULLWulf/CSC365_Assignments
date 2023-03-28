3e24e040ebe9fe6b874d46b5e5e3cb12b36dbc81
Nathan Wolf <nwolf2@oswego.edu>
2023-03-28 10:23:18 -0400

    added commentation

--- hashMap.go
+++ hashMap.go
@@ -7,6 +7,10 @@ import (
 	"os"
 )
 
+/*
+This Hashmap was used in Assignment 1 but is no used in Assignment 2
+*/
+
 const initialSize = 8 // initial Size of the hash map
 
 // Node represents a key-value pair stored in the hash map
